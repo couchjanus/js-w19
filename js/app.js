1f8ac50bdb2c363615c4bb8903708ef520edf833
couch Janus <couchjanus@gmail.com>
2020-09-19 17:59:01 +0300

    added unit 1

---
+++ js/app.js
@@ -0,0 +1,47 @@
+"use strict"
+// 
+const toggleBtn = document.querySelector(".cart-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+// function openCart() {
+//     sidebar.style.transform = 'translate(0)';
+// }
+      
+// function closeCart() {
+//     sidebar.style.transform = '';
+// }
+
+// closeBtn.onclick=closeCart;
+// toggleBtn.onclick=openCart;
+// ==============================
+// toggleBtn.addEventListener("click", function () {
+//   // if (sidebar.classList.contains("show-sidebar")) {
+//   //   sidebar.classList.remove("show-sidebar");
+//   // } else {
+//   //   sidebar.classList.add("show-sidebar");
+//   // }
+// });
+
+// closeBtn.addEventListener("click", function () {
+//   sidebar.classList.remove("show-sidebar");
+// });
+
+// ==============================
+// toggleBtn.addEventListener("click", function () {
+//   sidebar.classList.toggle("show-sidebar");
+// });
+
+// closeBtn.addEventListener("click", function () {
+//   sidebar.classList.remove("show-sidebar");
+// });
+// ==============================
+// (function(){
+//     toggleBtn.addEventListener("click", function () {
+//         sidebar.classList.toggle("show-sidebar");
+//     });
+
+//     closeBtn.addEventListener("click", function () {
+//         sidebar.classList.remove("show-sidebar");
+//     });
+// })();
